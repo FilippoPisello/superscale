899e08dc3074e664e5ac5c7037a335affdb146f9
Filippo <filippo.pisello@live.it>
2022-10-16 18:14:08 +0200

    fix: assert in test

--- measurehero/tests/test_measure_inference_int.py
+++ measurehero/tests/test_measure_inference_int.py
@@ -9,8 +9,8 @@ TEST_CASES = load_test_cases(LOCALIZATION_CODE_FR)
 
 
 @pytest.mark.parametrize(
-    ("input_string", "units"),
+    ("input_string", "expected_units"),
     TEST_CASES.units,
 )
-def test_units_extraction(input_string, units):
-    return ext.extract_units_info(input_string) == units
+def test_units_extraction(input_string, expected_units):
+    assert ext.extract_units(input_string) == expected_units
