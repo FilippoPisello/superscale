d8cb0d3dbcbe1ec3dbcfa0c08dd04895ad11e910
Filippo <filippo.pisello@live.it>
2022-10-22 14:46:00 +0200

    feat: add length x length support

--- measurehero/unit_of_measure.py
+++ measurehero/unit_of_measure.py
@@ -8,5 +8,8 @@ class UnitOfMeasure:
     ratio: int | float
     priority: int = 1
 
-    def is_piece_measure(self):
+    def is_piece_measure(self) -> bool:
         return self.convert_to == "piece"
+
+    def is_length_measure(self) -> bool:
+        return self.convert_to == "meter"
